f7bb4cf2e880f546de5f724cca94d835a93ea424
cry1s <cry1s@ya.ru>
2022-09-16 01:29:31 +0300

    lab1

---
+++ krainikov_rt5_31_lab1.py
@@ -0,0 +1,108 @@
+import sys
+import math
+
+def get_coef(index, prompt, error_prompt='Ошибка ввода. Повторите ввод'):
+    '''
+    Читаем коэффициент из командной строки или вводим с клавиатуры
+
+    Args:
+        index (int): Номер параметра в командной строке
+        prompt (str): Приглашение для ввода коэффицента
+
+    Returns:
+        float: Коэффициент квадратного уравнения
+    '''
+    try:
+        # Пробуем прочитать коэффициент из командной строки
+        coef_str = float(sys.argv[index])
+    except:
+        # Вводим с клавиатуры
+        print(prompt)
+        coef_str = input()
+    # Переводим строку в действительное число
+    while True:
+        try:
+            coef = float(coef_str)
+            break
+        except:
+            print(error_prompt)
+            coef_str = input()
+    return coef
+
+
+def get_square_roots(a, b, c):
+    '''
+    Вычисление корней квадратного уравнения
+
+    Args:
+        a (float): коэффициент А
+        b (float): коэффициент B
+        c (float): коэффициент C
+
+    Returns:
+        list[float]: Список корней
+    '''
+    result = []
+    D = b*b - 4*a*c
+    if D == 0.0:
+        root = -b / (2.0*a)
+        result.append(root)
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+        root1 = (-b + sqD) / (2.0*a)
+        root2 = (-b - sqD) / (2.0*a)
+        result.append(root1)
+        result.append(root2)
+    return result
+
+def get_bisquare_roots(a, b, c):
+    '''
+    Вычисление корней биквадратного уравнения
+
+    Args:
+        a (float): коэффициент А
+        b (float): коэффициент B
+        c (float): коэффициент C
+
+    Returns:
+        list[float]: Список корней
+    '''
+    result = []
+    square_roots = get_square_roots(a, b, c)
+    for root in square_roots:
+        if root == 0:
+            result.append(0)
+        if root > 0:
+            result.append(math.sqrt(root))
+            result.append(-math.sqrt(root))
+        
+    return result
+
+
+def main():
+    '''
+    Основная функция
+    '''
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    # Вычисление корней
+    roots = get_bisquare_roots(a,b,c)
+    # Вывод корней
+    len_roots = len(roots)
+    if len_roots == 0:
+        print('Нет корней')
+    elif len_roots == 1:
+        print('Один корень: {}'.format(roots[0]))
+    elif len_roots == 2:
+        print('Два корня: {} и {}'.format(roots[0], roots[1]))
+    elif len_roots == 3:
+        print('Три корня: {}, {} и {}'.format(roots[0], roots[1], roots[2]))
+    elif len_roots == 4:
+        print('Четыре корня: {}, {}, {} и {}'.format(roots[0], roots[1], roots[2], roots[3]))
+    
+if __name__ == '__main__':
+    main()
+
+# Пример запуска
+# krainikov_rt5_31_lab1.py 1 0 -16

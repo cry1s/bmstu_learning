5f816a8bf92f1a8ebcf9048065df94f6c601240b
cry1s <cry1s@ya.ru>
2022-10-14 14:09:20 +0300

    lab3

---
+++ lab3/lab_python_fp/gen_random.py
@@ -0,0 +1,11 @@
+import random
+# Пример:
+# gen_random(5, 1, 3) должен выдать выдать 5 случайных чисел
+# в диапазоне от 1 до 3, например 2, 2, 3, 2, 1
+# Hint: типовая реализация занимает 2 строки
+def gen_random(num_count, begin, end):
+    return [random.randint(begin, end) for i in range(num_count)]
+
+if __name__ == "__main__":
+    print(gen_random(5, 1, 3))
+    print(gen_random(5, 1, 3))
